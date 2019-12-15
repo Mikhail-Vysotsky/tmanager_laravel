8cfbfa23288e0b272a0511679d7e80cee74bec55
mikhail.vissotskiy <zubarester@gmail.com>
2019-12-16 01:15:56 +0700

    create migrations

---
+++ database/migrations/2019_12_15_100000_create_users_table.php
@@ -0,0 +1,21 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('post');
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
